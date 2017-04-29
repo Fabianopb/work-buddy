b494283416e6bba6a72a56ed9183330202e421de
Fabiano Brito <fabiano.brito@leadin.fi>
2017-04-29 11:48:08 +0900

    Creates basic calendar layout.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {List, ListItem} from 'material-ui/List';
 
 import './App.css';
 
@@ -10,7 +11,52 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div>stuff</div>
+        <div>
+          <div className="calendar-title">
+            <div className="weekday-title">Mon</div>
+            <div className="weekday-title">Tue</div>
+            <div className="weekday-title">Wed</div>
+            <div className="weekday-title">Thu</div>
+            <div className="weekday-title">Fri</div>
+          </div>
+            <div className="calendar-box">
+              <List className="weekday-table">
+                <ListItem primaryText="8:00" />
+                <ListItem primaryText="9:00" />
+                <ListItem primaryText="10:00" />
+                <ListItem primaryText="11:00" />
+                <ListItem primaryText="12:00" />
+              </List>
+              <List className="weekday-table">
+                <ListItem primaryText="8:00" />
+                <ListItem primaryText="9:00" />
+                <ListItem primaryText="10:00" />
+                <ListItem primaryText="11:00" />
+                <ListItem primaryText="12:00" />
+              </List>
+              <List className="weekday-table">
+                <ListItem primaryText="8:00" />
+                <ListItem primaryText="9:00" />
+                <ListItem primaryText="10:00" />
+                <ListItem primaryText="11:00" />
+                <ListItem primaryText="12:00" />
+              </List>
+              <List className="weekday-table">
+                <ListItem primaryText="8:00" />
+                <ListItem primaryText="9:00" />
+                <ListItem primaryText="10:00" />
+                <ListItem primaryText="11:00" />
+                <ListItem primaryText="12:00" />
+              </List>
+              <List className="weekday-table">
+                <ListItem primaryText="8:00" />
+                <ListItem primaryText="9:00" />
+                <ListItem primaryText="10:00" />
+                <ListItem primaryText="11:00" />
+                <ListItem primaryText="12:00" />
+              </List>
+            </div>
+        </div>
       </MuiThemeProvider>
     );
   }
